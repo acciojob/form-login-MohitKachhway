7809f04ca5aec3b2f2cd637eb9ac751ab367ba33
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-12 18:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
-
+    let form = document.getElementById("form1");
+    let firstName = form.fname.value;
+    let secondName = form.lname.value;
+  
+  alert(`${firstName} ${secondName}`);
+  event.preventDefault();
 }
